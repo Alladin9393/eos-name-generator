0de25632eb361ef9b036a69781562e7f3d755a17
Vlad Pavlichek <ember.toon@protonmail.com>
2020-05-23 00:22:05 +0300

    Create project structure (#1)
    * Addded base dependencies
    
    * Create base struct of project and CI/CD config
    
    * Added config for linters and module description
    
    * Added setup config

---
+++ eos_name_generator/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level module for `EOS` name generator.
+
+This module generate random name which suits for `eosio blockchain` name
+conversation.
+
+The module provides 3 methods to generate a name:
+    - random name generation based in a ready-made data (word dictionary)
+    - markov chain text generation
+    - recurrent neural network text generation
+"""
