b8dd862a880b9ab331ba5b287ce664b7c4d783c2
Vlad Pavlichek <ember.toon@protonmail.com>
2020-05-24 17:04:24 +0300

    added version to module (#6)
    * Added `__version__` and `__version_info__` to the top module eos_name_generator.

--- eos_name_generator/__init__.py
+++ eos_name_generator/__init__.py
@@ -9,3 +9,8 @@ The module provides 3 methods to generate a name:
     - recurrent neural network text generation
 """
 from eos_name_generator.random_generator.generator import RandomNameGenerator
+
+__version__ = "0.1.0"
+__version_info__ = tuple(
+    int(i) for i in __version__.split(".") if i.isdigit()
+)
